6a73672bf1d0e746f050e614a789481c0a183e2b
StephaneLay <stephane.lay@hotmail.fr>
2025-08-15 15:11:55 +0200

    ended gameplay

--- script.js
+++ script.js
@@ -22,71 +22,91 @@ const GRID = {};
 
 let tileNumber = 0
 
-//MAUVCAISE GRILLEs
-for (let i = 0; i < 4; i++) {
-    for (let j = 3; j >-1; j--) {
-        GRID[`${i},${j}`] = tiles[tileNumber];
+for (let i = 3; i > -1; i--) {
+    for (let j = 0; j < 4; j++) {
+        GRID[`${j},${i}`] = tiles[tileNumber];
         tileNumber++;
     }
-    
 }
-
-console.log(GRID);
 
 //Lancement de jeu
 spawnTile();
-
 
 //inputs
 document.addEventListener('keydown', function (event) {
     event.preventDefault();
     switch (event.key) {
         case 'ArrowRight':
-            processMovement(3, null, -1); // x : down , y : down
+            processMovement('desc', 'desc', false);
             break;
         case 'ArrowLeft':
-            processMovement(0, null, +1); // EXEMPLE de GRID = sur x : up (boucle increment), sur y : down(boucle decrement)
+            processMovement('desc', 'asc', false);
             break;
         case 'ArrowUp':
-            processMovement(null, 3, -1); // x : down , y: up  
+            processMovement('asc', 'desc', true);
             break;
         case 'ArrowDown':
-            processMovement(null, 0, +1); // x : up , y : up
+            processMovement('asc', 'asc', true);
             break;
         default:
             return;
     }
 })
 
-//En priorité : ============> refaire bien la grille , on ne recupere pas les coordonnées dans le bon sens des axes qu'on aime
 
-//EN FAIT ICI ON A LES BONNES CHOSES :
 
-//ON DOIT INCREMENTER LES VALEURS DE SORTE A CE QU'ON REPERE LES BONNES DE DEPART , PUIS QU'ON PARCOUT LES LIGNES DANS LE BON SENS
-// ICI ON RECUPRE BIEN ELS BONNES VALEURS DE DEPART , MAIS ON NE SAIT PAS DANS QUEL SENS ET SUR QUEL AXE LES INCREMENTER 
-// A FIND OUT MAIS SUREMENT POSSIBLE DE FAIRE QUELQUE CHOSE DE BIEN MIEUX A PARTIR DES CALLBACKS PROCESSMOVEMENTS (BOUCLE SWITCH)
+// La plus complexe : le behavior des tiles selon les deplacements inputs par l'user
+// D'abord on doit recuperer un système de lignes dans la bonne direction , pour ca on doit avoir deux boucles for qui s'incremente/decremente dans le bon sens
+// Le but , c'est qu'elles fournissent , dans l'ordre exact , les coordonnées des tuiles sur lesquelles on va iterer
+// Une fois ces clés faites, on ignore la premiere de chaque ligne mais on la garde en mémoire
+// Ensuite, on cherche à recuperer la tuile la plus proche de celle sur laquelle on itere actuellement.
+// Si elle n'existe pas : on la déplace direct sur la premiere case de la ligne.
+// Si elle existe , on verifie si elles doivent fusionner ( meme valeur)
+// Si oui, on les fusionne , si non , on déplace notre tuile juste "derriere"(dans le sens adéquat).
+function processMovement(loop1, loop2, regularOrder) {
+    for (let i = loop1 == 'asc' ? 0 : 3; loop1 == 'asc' ? i < 4 : i > -1; loop1 == 'asc' ? i++ : i--) {
+        let currentLineKeys = []
+        for (let j = loop2 == 'asc' ? 0 : 3; loop2 == 'asc' ? j < 4 : j > -1; loop2 == 'asc' ? j++ : j--) {
+            let key = regularOrder ? `${i},${j}` : `${j},${i}`;
+            if (currentLineKeys.length > 0 && GRID[key].textContent !== '') {
+                let keysByClosest = [];
+                for (const currentKey of currentLineKeys) {
+                    keysByClosest.unshift(currentKey);
+                }
+                let closestTile;
+                let closestTileKey;
+                for (const tileKey of keysByClosest) {
+                    if (GRID[tileKey].textContent !== '') {
+                        closestTile = GRID[tileKey];
+                        closestTileKey = tileKey;
+                        break;
+                    }
+                }
+                if (closestTile === undefined) {
+                    console.log('move', key, currentLineKeys[0])
+                    moveTile(GRID[key], GRID[currentLineKeys[0]]);
+                } else {
+                    if (closestTile.textContent === GRID[key].textContent) {
+                        console.log('merge', key, closestTile)
+                        mergeTiles(GRID[key], closestTile);
 
-// On devrait , en s'inspirant de la boucle qui recupere les grid( qui sort d'ailleurs la bonne suite pour LEFT), reussir notre algo
-// Nomenclature sur inputs !!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    } else {
+                        closestNeighbourKey = currentLineKeys[currentLineKeys.indexOf(closestTileKey) + 1];
+                        if (closestNeighbourKey) {
+                            console.log('moveclosestNeighbourKey', key, closestNeighbourKey)
+                            moveTile(GRID[key], GRID[closestNeighbourKey])
+                        }
 
-function processMovement(x, y, value) {
-    let previousKeys= []
-    for (let i = value > 0 ? 0 : 3; value > 0 ? i < 4 : i > -1; value > 0 ? i++ : i--) {
-        let key = `${x ?? i},${y ?? i}`;
 
-        //Traitement des tiles => debut d'algo cf CAHIER !
+                    }
+                }
 
-        // if (previousKeys.length > 0) {
-        //     for (const key of previousKeys) {
-        //         if (GRID[key]) {
-                    
-        //         }
-        //     }
-        // }
-        previousKeys.push(key);
-        
+            }
+            currentLineKeys.push(key);
+        }
     }
-    console.log(previousKeys);
+    spawnTile();
+
 }
 
 function spawnTile() {
@@ -97,6 +117,10 @@ function spawnTile() {
         }
     }
 
+    if (emptyTiles.length <1) {
+        endGame();
+    }
+
     let spawnValue = Math.random() < 0.90 ? '2' : '4';
     setTile(emptyTiles[Math.floor(Math.random() * emptyTiles.length)], spawnValue);
 }
@@ -104,4 +128,26 @@ function spawnTile() {
 function setTile(tile, value) {
     tile.textContent = value;
     tile.style.backgroundColor = COLORS[value];
+}
+
+function moveTile(originTile, destinationTile) {
+    setTile(destinationTile, originTile.textContent);
+    setTile(originTile, "");
+}
+
+function mergeTiles(originTile, destinationTile) {
+    let value = parseInt(destinationTile.textContent);
+    let newValue = (value * 2).toString();
+    setTile(destinationTile, newValue);
+    setTile(originTile, "");
+}
+
+
+//ON DOIT RANGER UN PEU POUR ANNULER L'EVENT DE KEYDOWN , MAIS POUR CA ON DOIT NOMMER LA FONCTION DE DOCUMENT EVENT LISTENER
+function endGame(){
+    for (const tile of tiles) {
+        tiles.textContent = "*";
+        // document.removeEventListener('keydown',function);
+    }
+    message.textContent = "You lose";
 }
